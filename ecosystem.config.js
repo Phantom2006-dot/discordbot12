4cf8fb899a9b759501b6621351f7657f0706f4f9
Phantom2006-dot <danielnworah2006@gmail.com>
2025-11-26 11:08:05 +0100

    Add files via upload

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [
+    {
+      name: "Global Prime Discord Bot",
+      script: "js/globalprimediscordbot.js",
+      watch: true,
+      ignore_watch: ["node_modules", "logs"],
+    },
+  ],
+};
